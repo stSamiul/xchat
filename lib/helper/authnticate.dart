e8b3b2646c9763f761dd45a32e79c053a5221e95
Samiul Hossain <47440758+stSamiul@users.noreply.github.com>
2021-04-21 23:17:32 +0600

    d21

---
+++ lib/helper/authnticate.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Authnticate extends StatefulWidget {
+  @override
+  _AuthnticateState createState() => _AuthnticateState();
+}
+
+class _AuthnticateState extends State<Authnticate> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
