e9107172bc9722ff6104d2b07482775894e7ee07
Samiul Hossain <47440758+stSamiul@users.noreply.github.com>
2021-04-23 17:16:32 +0600

    d23

--- lib/inside/search.dart
+++ lib/inside/search.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:xchat/inside/widgets/widget.dart';
 
 class SearchScren extends StatefulWidget {
   @override
@@ -8,7 +11,33 @@ class SearchScren extends StatefulWidget {
 class _SearchScrenState extends State<SearchScren> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        color: Colors.grey[50],
+        child: Column(
+          children: [
+            Container(
+              
+              padding: EdgeInsets.symmetric(horizontal: 25,vertical: 14),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "user name...",
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                        )
+                      ),
+                    )),
+                    Image.asset("assets/images/search.png")
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
       
     );
   }
